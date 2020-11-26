9c80d9306eb88f21ba5fce98d341f5d152b24809
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-11-26 07:50:14 -0700

    Abstract into module, adding the current directory to the path in order to import a local module is currently done in the script

--- asdf.py
+++ asdf.py
@@ -1,30 +1,10 @@
-from ctypes import *
+import sys
 
-class Asdf(Structure):
-    _fields_ = [
-        ("_loc", c_float * 2)
-    ]
+sys.path.insert(1, '.')
 
-    def update(self, loc):
-        self._loc = loc
+import omni
 
-        set(self._ptr, self._loc)
-
-    def __init__(self, ptr):
-        self._ptr = cast(ptr, POINTER(Asdf))
-
-        self.update(self._ptr.contents._loc)
-
-lib = CDLL('libhjkl.so')
-
-set = lib.set
-set.restype = c_void_p
-set.argtypes = [
-    POINTER(Asdf),
-    c_float * 2
-]
-
-asdf = Asdf(lib.asdf)
+asdf = omni.Asdf(omni.lib.asdf)
 
 for i in range(0, 2):
     asdf._loc[i] += 1
---
+++ omni.py
@@ -0,0 +1,25 @@
+from ctypes import *
+
+class Asdf(Structure):
+    _fields_ = [
+        ("_loc", c_float * 2)
+    ]
+
+    def update(self, loc):
+        self._loc = loc
+
+        set(self._ptr, self._loc)
+
+    def __init__(self, ptr):
+        self._ptr = cast(ptr, POINTER(Asdf))
+
+        self.update(self._ptr.contents._loc)
+
+lib = CDLL('libhjkl.so')
+
+set = lib.set
+set.restype = c_void_p
+set.argtypes = [
+    POINTER(Asdf),
+    c_float * 2
+]
