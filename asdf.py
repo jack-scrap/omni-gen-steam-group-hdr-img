ff7597513e626d415070b532daa250f267da325f
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-11-26 21:44:29 -0700

    Squash, handle keyboard input in detached thread. Part of script causes memory error for undetermined reasons, has been omitted

--- asdf.py
+++ asdf.py
@@ -4,10 +4,10 @@ sys.path.insert(1, '.')
 
 import omni
 
-asdf = omni.Asdf(omni.lib.asdf)
+# asdf = omni.Asdf(omni.lib.asdf)
 
-for _ in range(0, 7):
-    asdf.update([
-        asdf._loc[0] + 0.1,
-        asdf._loc[1] + 0.1
-    ])
+# for _ in range(0, 3):
+#     asdf.update([
+#         asdf._loc[0] + 0.1,
+#         asdf._loc[1] + 0.1
+#     ])
--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <Python.h>
 #include <dlfcn.h>
+#include <thread>
 
 #include "hjkl.h"
 #include "util.h"
 
-int main() {
-	Py_Initialize();
+bool run = true;
 
-	std::string buff = rd("asdf.py");
+std::string buff = rd("asdf.py");
 
-	void* handle = dlopen("./libasdf.so", RTLD_LAZY);
-
-	void (*asdfDraw)(Asdf* asdf) = (void (*)(Asdf*)) dlsym(handle, "asdfDraw");
-
-	SDL_Event e;
-	while (true) {
+SDL_Event e;
+void kb() {
+	while (run) {
 		while (SDL_PollEvent(&e)) {
 			if (e.type == SDL_KEYDOWN) {
 				if (e.key.keysym.sym == SDLK_F1) {
@@ -24,10 +21,23 @@ int main() {
 			}
 
 			if (e.type == SDL_QUIT) {
-				SDL_Quit();
+				run = false;
 			}
 		}
+	}
+}
 
+int main() {
+	Py_Initialize();
+
+	void* handle = dlopen("./libasdf.so", RTLD_LAZY);
+
+	void (*asdfDraw)(Asdf* asdf) = (void (*)(Asdf*)) dlsym(handle, "asdfDraw");
+
+	std::thread input(kb);
+	input.detach();
+
+	while (run) {
 		disp.clear(0.16, 0.16, 0.16);
 
 		asdfDraw(&asdf);
