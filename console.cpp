e85976c57acc62f52b228217a6974443136fa570
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-12-16 14:42:32 -0700

    Squash

--- console.cpp
+++ console.cpp
@@ -58,7 +58,7 @@ void Console::render() {
 		for (int i = 0; i < _buff[l].size(); i++) {
 			if (_active[l][i]) {
 				SDL_Surface* bg = SDL_CreateRGBSurface(0, bgRect.w, bgRect.h, 4 * sizeof (long int), 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
-				SDL_FillRect(bg, &bgRect, SDL_MapRGBA(bg->format, col[true][0], col[true][1], col[true][2], 255));
+				SDL_FillRect(bg, &bgRect, SDL_MapRGBA(bg->format, col[_active[l][i]][0], col[_active[l][i]][1], col[_active[l][i]][2], 255));
 
 				glTexSubImage2D(GL_TEXTURE_2D, 0, i * charRect.w, l * charRect.h, charRect.w, charRect.h, GL_BGRA, GL_UNSIGNED_BYTE, bg->pixels);
 
