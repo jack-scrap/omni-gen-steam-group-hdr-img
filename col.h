8ad43e5b8c335d7555122687e4f08fbbcb80c3a3
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-12-16 12:54:07 -0700

    Refactored, redundant declaration of data type for color was eliminated

--- col.h
+++ col.h
@@ -7,11 +7,3 @@ inline glm::vec3 col[2] = {
 	glm::vec3(38, 38, 38),
 	glm::vec3(255, 155, 71)
 };
-
-inline SDL_Color colS[2] = {
-	{
-		38, 38, 38, 255
-	}, {
-		255, 155, 71, 255
-	}
-};
--- console.cpp
+++ console.cpp
@@ -16,7 +16,7 @@ Console::Console(std::vector<std::string> buff) :
 		TTF_Font* font = TTF_OpenFont("res/terminus.bdf", state::sz[1]);
 
 		SDL_Surface* bg = SDL_CreateRGBSurface(0, bgRect.w, bgRect.h, 4 * sizeof (long int), 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000);
-		SDL_FillRect(bg, &bgRect, SDL_MapRGBA(bg->format, colS[false].r, colS[false].g, colS[false].b, 255));
+		SDL_FillRect(bg, &bgRect, SDL_MapRGBA(bg->format, col[false][0], col[false][1], col[false][2], 255));
 
 		for (int l = 0; l < _buff.size(); l++) {
 			std::vector<bool> line;
@@ -34,11 +34,7 @@ Console::Console(std::vector<std::string> buff) :
 			std::vector<SDL_Surface*> line;
 
 			for (int i = 0; i < _buff[l].size(); i++) {
-				line.push_back(TTF_RenderGlyph_Blended(
-					font,
-					_buff[l][i],
-					colS[_active[l][i]]
-				));
+				line.push_back(TTF_RenderGlyph_Blended(font, _buff[l][i], {col[_active[l][i]][0], col[_active[l][i]][1], col[_active[l][i]][2]}));
 			}
 
 			map.push_back(line);
@@ -52,7 +48,7 @@ Console::Console(std::vector<std::string> buff) :
 			for (int i = 0; i < _buff[l].size(); i++) {
 				if (_active[l][i]) {
 					SDL_Surface* bg = SDL_CreateRGBSurface(0, bgRect.w, bgRect.h, 4 * sizeof (long int), 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
-					SDL_FillRect(bg, &bgRect, SDL_MapRGBA(bg->format, colS[true].r, colS[true].g, colS[true].b, 255));
+					SDL_FillRect(bg, &bgRect, SDL_MapRGBA(bg->format, col[true][0], col[true][1], col[true][2], 255));
 
 					glTexSubImage2D(GL_TEXTURE_2D, 0, i * charRect.w, l * charRect.h, charRect.w, charRect.h, GL_BGRA, GL_UNSIGNED_BYTE, bg->pixels);
 
