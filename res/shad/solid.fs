bfdd733b5b76e6d696f7a41765209e983ffc6d08
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-01-06 12:11:46 -0700

    Utilized uniform properly instead of static value

--- res/shad/solid.fs
+++ res/shad/solid.fs
@@ -1,10 +1,12 @@
 #version 400
 
+uniform bool active;
+
 vec3[2] col = vec3[2](
 	vec3(38, 38, 38),
 	vec3(255, 155, 71)
 );
 
 void main() {
-	gl_FragColor = vec4(col[int(true)] / 255.0, 1.0);
+	gl_FragColor = vec4(col[int(active)] / 255.0, 1.0);
 }
