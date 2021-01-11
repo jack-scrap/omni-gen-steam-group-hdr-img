d12aa5bc47b8c9e2515b75c0783983c688d86e20
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-01-10 18:01:24 -0700

    Placed variables and data related to the camera in the library

--- cam.h
+++ cam.h
@@ -6,4 +6,13 @@ struct Cam {
 	glm::vec3
 		_pos,
 		_scale;
+
+	bool _drag;
+
+	glm::vec3 _prev;
+
+	int
+		_begin[2],
+		_curr[2],
+		_delta[2];
 };
--- main.cpp
+++ main.cpp
@@ -13,16 +13,7 @@
 #include "pt.h"
 #include "cam.h"
 
-bool
-	run,
-	drag;
-
-glm::vec3 prev;
-
-int
-	begin[2],
-	curr[2],
-	delta[2];
+bool run;
 
 Console* console;
 
@@ -70,26 +61,26 @@ int main() {
 			}
 
 			if (e.type == SDL_MOUSEBUTTONDOWN) {
-				drag = true;
+				cam._drag = true;
 
-				SDL_GetMouseState(&begin[0], &begin[1]);
+				SDL_GetMouseState(&cam._begin[0], &cam._begin[1]);
 
-				prev = cam._pos;
+				cam._prev = cam._pos;
 			}
 
 			if (e.type == SDL_MOUSEBUTTONUP) {
-				drag = false;
+				cam._drag = false;
 			}
 
 			if (e.type == SDL_MOUSEMOTION) {
-				if (drag) {
-					SDL_GetMouseState(&curr[0], &curr[1]);
+				if (cam._drag) {
+					SDL_GetMouseState(&cam._curr[0], &cam._curr[1]);
 
-					delta[0] = begin[0] - curr[0];
-					delta[1] = begin[1] - curr[1];
+					cam._delta[0] = cam._begin[0] - cam._curr[0];
+					cam._delta[1] = cam._begin[1] - cam._curr[1];
 
-					cam._pos[0] = prev.x + delta[0];
-					cam._pos[2] = prev.y + delta[1];
+					cam._pos[0] = cam._prev.x + cam._delta[0];
+					cam._pos[2] = cam._prev.y + cam._delta[1];
 				}
 			}
 
