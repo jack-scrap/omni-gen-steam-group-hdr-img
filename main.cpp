b83e5f70b7a438dabce545bd07f31a1a9bcf438e
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-01-31 06:39:07 -0700

    Rectified panning

--- main.cpp
+++ main.cpp
@@ -264,13 +264,14 @@ int main() {
 					cam._delta[0] = cam._curr[0] - cam._begin[0];
 					cam._delta[1] = -(cam._curr[1] - cam._begin[1]);
 
-					// X
-					cam._pos[0] = cam._prev[0] - cam._delta[0];
-					cam._pos[2] = cam._prev[2] + cam._delta[0];
+					cam._pos[0] = cam._prev[0];
+					cam._pos[2] = cam._prev[2];
 
-					// Y
-					cam._pos[0] = cam._prev[0] + cam._delta[1];
-					cam._pos[2] = cam._prev[2] + cam._delta[1];
+					cam._pos[0] += -cam._delta[0];
+					cam._pos[2] += cam._delta[0];
+
+					cam._pos[0] += cam._delta[1];
+					cam._pos[2] += cam._delta[1];
 				}
 			}
 
