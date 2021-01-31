7185bbcdf5a21f15d2402047781a95bf83ccc103
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-01-31 05:13:05 -0700

    Squash, attemptingt to rectify panning of camera

--- main.cpp
+++ main.cpp
@@ -264,9 +264,13 @@ int main() {
 					cam._delta[0] = cam._curr[0] - cam._begin[0];
 					cam._delta[1] = -(cam._curr[1] - cam._begin[1]);
 
-					cam._pos[0] = cam._prev[0] + cam._delta[0];
-					cam._pos[1] = cam._prev[1] + cam._delta[1];
-					cam._pos[2] = cam._prev[2] - cam._delta[0];
+					// X
+					cam._pos[0] = cam._prev[0] - cam._delta[0];
+					cam._pos[2] = cam._prev[2] + cam._delta[0];
+
+					// Y
+					cam._pos[0] = cam._prev[0] + cam._delta[1];
+					cam._pos[2] = cam._prev[2] + cam._delta[1];
 				}
 			}
 
