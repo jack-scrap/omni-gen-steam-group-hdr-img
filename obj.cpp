c5b6114b966e50fc8ca5d375807dd9bb538aacdd
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-02-13 14:08:56 -0700

    Temporarily rectified transformation of child nodes just by updating both the model and the accumulated matrix

--- obj.cpp
+++ obj.cpp
@@ -525,7 +525,7 @@ void objAnim(Obj* obj, glm::vec3 loc, glm::vec3 rot) {
 		d = util::matr::rot(d, rotInc);
 
 		obj->_model *= d;
-		objAcc(obj, glm::mat4(1.0));
+		obj->_acc *= d;
 
 		locFrame += glm::abs(locInc);
 		rotFrame += glm::abs(rotInc);
