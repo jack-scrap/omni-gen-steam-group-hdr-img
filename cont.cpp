3a8134a9a78af39431c82836bfffa128a68f6bfc
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-03-04 11:18:05 -0700

    Refactored

--- cont.cpp
+++ cont.cpp
@@ -4,9 +4,9 @@
 Cont* contMk(char c, glm::vec3 loc, glm::vec3 rot) {
 	Cont* _ = (Cont*) malloc(sizeof (Cont));
 
-	_->_parent = objMk("container_2x4", "obj", "dir", true, loc, rot);
-
 	_->_c = c;
+
+	_->_parent = objMk("container_2x4", "obj", "dir", true, loc, rot);
 
 	return _;
 }
