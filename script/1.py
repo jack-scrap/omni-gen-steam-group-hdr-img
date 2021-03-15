d38b4c0afc02011c88ee5130402f39037d92956f
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-03-14 22:49:25 -0600

    Populated other script with commands just for the sake of convenience

--- script/1.py
+++ script/1.py
@@ -1,1 +1,19 @@
 # Move the container into the other index
+
+import sys
+
+sys.path.insert(1, '.')
+
+from omni import *
+
+crane.ped(-1.0)
+
+crane.grab()
+
+crane.zoom(1.0)
+
+crane.zoom(-1.0)
+
+crane.grab()
+
+crane.zoom(-1.0)
