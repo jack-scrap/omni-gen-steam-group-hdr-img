317218aeb854901cbca96d4545a0f618360b24cd
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-04-01 21:25:50 -0600

    Squash

--- script/init/0/main.py
+++ script/init/0/main.py
@@ -2,4 +2,30 @@
 
 from omni import *
 
-asdf
+i = 0
+while i < 10:
+	crane.ped(-1.0)
+
+	i += 1
+
+i = 0
+while i < 6:
+	crane.zoom(1)
+
+	i += 1
+
+crane.grab()
+
+i = 0
+while i < 6:
+	crane.zoom(-1)
+
+	i += 1
+
+crane.grab()
+
+i = 0
+while i < 3:
+	crane.zoom(-1)
+
+	i += 1
