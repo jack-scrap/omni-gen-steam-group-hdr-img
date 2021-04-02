8d579b5a44aefa0667c4c39203826bc4a3be062e
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-04-02 14:26:33 -0600

    Squash

--- script/init/6/main.py
+++ script/init/6/main.py
@@ -1,31 +1,10 @@
-# Move the container into the index
+# Move the containers in a checkerboard pattern
 
 from omni import *
-
 i = 0
-while i < 10:
-	crane.ped(-1.0)
+j = 0
+while i < 3
+    while j < 3
+        j += 1
 
-	i += 1
-
-i = 0
-while i < 6:
-	crane.zoom(1)
-
-	i += 1
-
-crane.grab()
-
-i = 0
-while i < 6:
-	crane.zoom(-1)
-
-	i += 1
-
-crane.grab()
-
-i = 0
-while i < 3:
-	crane.zoom(-1)
-
-	i += 1
+    i += 1
