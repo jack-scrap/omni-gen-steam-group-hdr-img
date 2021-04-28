8d0a7404486562c69260f5e227045d31f1eebdc2
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-04-28 13:03:49 -0600

    Formatted

--- truck.h
+++ truck.h
@@ -21,7 +21,7 @@ typedef struct Truck {
 
 Truck* truckMk(glm::vec3 loc = glm::vec3(0.0), glm::vec3 rot = glm::vec3(0.0, 0.0, 0.0));
 
-Truck* truckMk(char* data, unsigned int ptr, glm::vec3 loc = glm::vec3(0.0), glm::vec3 rot = glm::vec3(0.0, 0.0, 0.0));
+Truck* truckMk(char* data, unsigned int sz, glm::vec3 loc = glm::vec3(0.0), glm::vec3 rot = glm::vec3(0.0, 0.0, 0.0));
 
 extern "C" void truckTurn(Truck* truck, float delta);
 
