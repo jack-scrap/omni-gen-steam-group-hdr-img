ddfd5d51c526e692a17c6f20b337dc0b25ea8b19
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-05-15 07:38:07 -0600

    Added object constructors creating and uploading texture, gets drawn exactly once before the console is re-rendered

--- scn.cpp
+++ scn.cpp
@@ -534,6 +534,56 @@ void scn::init(unsigned int stage, unsigned int lvl) {
 		mesh.push_back(obj);
 	}
 
+	GLfloat vtc[2 * 2 * 2 * 3];
+	int i = 0;
+	for (int z = 0; z < 2; z++) {
+		for (int y = 0; y < 2; y++) {
+			for (int x = 0; x < 2; x++) {
+				vtc[i] = x ? 1 : -1;
+				vtc[i + 1] = y ? 1 : -1;
+				vtc[i + 2] = z ? 1 : -1;
+
+				i += 3;
+			}
+		}
+	}
+
+	GLfloat st[] = {
+		0, 0,
+		0, 1,
+		1, 0,
+		1, 1,
+
+		0, 0,
+		0, 1,
+		1, 0,
+		1, 1
+	};
+
+	GLushort idc[3 * 2 * 3 * 2] = {
+		0, 1, 2,
+		2, 1, 3,
+
+		4, 5, 6,
+		6, 5, 7,
+
+		0, 4, 1,
+		1, 4, 5,
+
+		2, 6, 3,
+		3, 6, 7,
+
+		0, 4, 2,
+		2, 4, 6,
+
+		1, 5, 3,
+		3, 5, 7
+	};
+
+	Obj* obj = objMk(vtc, sizeof vtc / sizeof *vtc, st, sizeof st / sizeof *st, idc, sizeof idc / sizeof *idc, "obj", "tex", true);
+
+	mesh.push_back(obj);
+
 	// bound
 	boundRng = (Lim**) malloc(0);
 	noBoundRng = 0;
