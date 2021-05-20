fd1fd7692cd055458f1ae76a0b81c5cf48735a92
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-05-20 01:59:11 -0600

    Added static component to street light, rectified offset of light

--- street_light.cpp
+++ street_light.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 
 #include "street_light.h"
+#include "layout.h"
 
 StreetLight* streetLightMk(bool* pass, unsigned int no, glm::vec3 loc, glm::vec3 rot) {
 	StreetLight* _ = (StreetLight*) malloc(sizeof (StreetLight));
@@ -13,7 +14,7 @@ StreetLight* streetLightMk(bool* pass, unsigned int no, glm::vec3 loc, glm::vec3
 
 	Obj* child[_->_no];
 	for (int i = 0; i < _->_no; i++) {
-		child[i] = objMk("go", "obj", "solid", true, glm::vec3(i * 2.0, 6.0 + 1.0, 0.0), glm::vec3(0.0, M_PI / 2, 0.0));
+		child[i] = objMk("go", "obj", "solid", true, glm::vec3(-((0.2 / 2) + (layout::pad * 2)), 6.0 + 1.0, i * 2.0), glm::vec3(0.0, M_PI / 2, 0.0));
 	}
 
 	_->_parent = objMk("street_light", "obj", "dir", false, child, sizeof child / sizeof *child, loc, rot);
