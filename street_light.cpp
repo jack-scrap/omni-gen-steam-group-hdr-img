267d41a3264d134094c30ec136f3a5fba58a96bc
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-05-20 01:42:31 -0600

    Padded street light vertically

--- street_light.cpp
+++ street_light.cpp
@@ -13,7 +13,7 @@ StreetLight* streetLightMk(bool* pass, unsigned int no, glm::vec3 loc, glm::vec3
 
 	Obj* child[_->_no];
 	for (int i = 0; i < _->_no; i++) {
-		child[i] = objMk("go", "obj", "solid", true, glm::vec3(i * 2.0, 6.0, 0.0), glm::vec3(0.0, M_PI / 2, 0.0));
+		child[i] = objMk("go", "obj", "solid", true, glm::vec3(i * 2.0, 6.0 + 1.0, 0.0), glm::vec3(0.0, M_PI / 2, 0.0));
 	}
 
 	_->_parent = objMk("street_light", "obj", "dir", false, child, sizeof child / sizeof *child, loc, rot);
