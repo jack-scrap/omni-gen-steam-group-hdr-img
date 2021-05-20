8c2bfd261311b8d801264baef17a4428bf3aff81
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-05-19 18:25:54 -0600

    Refactored, formatted

--- dict.cpp
+++ dict.cpp
@@ -10,8 +10,17 @@
 Dict* dictMk(Var** data, unsigned int* type, unsigned int no, std::string name, glm::vec3 loc, glm::vec3 rot) {
 	Dict* _ = (Dict*) malloc(sizeof (Dict));
 	
-	Obj* child[1];
+	Obj* child[1 + no];
 
+	// identifier
+	if (!name.empty()) {
+		Str* id = strMk(name, glm::vec3(0.0, 0.0, -(layout::margin * 2)));
+		child[0] = id->_parent;
+	} else {
+		child[0] = nullptr;
+	}
+
+	// data
 	for (int i = 0; i < no; i++) {
 		Var* var = (Var*) data[i];
 
@@ -28,14 +37,6 @@ Dict* dictMk(Var** data, unsigned int* type, unsigned int no, std::string name, 
 				break;
 			}
 		}
-	}
-
-	// identifier
-	if (!name.empty()) {
-		Str* id = strMk(name, glm::vec3(0.0, 0.0, -(layout::margin * 2)));
-		child[0] = id->_parent;
-	} else {
-		child[0] = nullptr;
 	}
 
 	// scope
