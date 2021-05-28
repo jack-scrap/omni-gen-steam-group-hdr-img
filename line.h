3c423e576174a832e65794f2668beb94067919b4
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-05-28 11:00:06 -0600

    Made line indices static

--- line.h
+++ line.h
@@ -4,6 +4,10 @@
 
 #include "obj.h"
 
+typedef struct Line {
+	static GLushort _idc[2];
+} Line;
+
 Obj* lineMk(GLfloat* vtc, std::string nameVtx, std::string nameGeom, std::string nameFrag, bool active, glm::vec3 loc = glm::vec3(0.0), glm::vec3 rot = glm::vec3(0.0));
 
 void lineDraw(Obj* line);
