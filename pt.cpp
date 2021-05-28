36e961a39d687a6dbae5b78d38acd044b4035f45
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-05-28 11:52:40 -0600

    Formatted

--- pt.cpp
+++ pt.cpp
@@ -10,29 +10,29 @@ Obj* ptMk(GLfloat* vtc, unsigned int noVtc, GLushort* idc, unsigned int noIdc, s
 	return objMk(vtc, sizeof vtc / sizeof *vtc, idc, sizeof idc / sizeof *idc, nameVtx, nameGeom, nameFrag, active, loc, rot);
 }
 
-void ptDraw(Obj* obj) {
-	obj->_view = glm::lookAt(cam._pos, cam._pos + glm::vec3(1000.0, -1000.0, 1000.0), glm::vec3(0, 1, 0));
-	obj->_view = glm::scale(obj->_view, cam._scale);
+void ptDraw(Obj* pt) {
+	pt->_view = glm::lookAt(cam._pos, cam._pos + glm::vec3(1000.0, -1000.0, 1000.0), glm::vec3(0, 1, 0));
+	pt->_view = glm::scale(pt->_view, cam._scale);
 
-	glBindVertexArray(obj->_mesh->_id[Mesh::VAO]);
-	obj->_prog.use();
+	glBindVertexArray(pt->_mesh->_id[Mesh::VAO]);
+	pt->_prog.use();
 
-	glUniformMatrix4fv(obj->_uni[Obj::MODEL], 1, GL_FALSE, glm::value_ptr(obj->_acc));
-	glUniformMatrix4fv(obj->_uni[Obj::VIEW], 1, GL_FALSE, glm::value_ptr(obj->_view));
+	glUniformMatrix4fv(pt->_uni[Obj::MODEL], 1, GL_FALSE, glm::value_ptr(pt->_acc));
+	glUniformMatrix4fv(pt->_uni[Obj::VIEW], 1, GL_FALSE, glm::value_ptr(pt->_view));
 
-	glUniform1ui(obj->_uni[Obj::T], obj->_t);
+	glUniform1ui(pt->_uni[Obj::T], pt->_t);
 
-	glUniform1ui(obj->_uni[Obj::ACTIVE], obj->_active);
+	glUniform1ui(pt->_uni[Obj::ACTIVE], pt->_active);
 
-	glDrawElements(GL_POINTS, obj->_mesh->_noIdc, GL_UNSIGNED_SHORT, (GLvoid*) 0);
+	glDrawElements(GL_POINTS, pt->_mesh->_noIdc, GL_UNSIGNED_SHORT, (GLvoid*) 0);
 
-	obj->_prog.unUse();
+	pt->_prog.unUse();
 	glBindVertexArray(0);
 
-	obj->_t++;
-	for (int i = 0; i < obj->_noChild; i++) {
-		if (obj->_child[i]) {
-			objDraw(obj->_child[i]);
+	pt->_t++;
+	for (int i = 0; i < pt->_noChild; i++) {
+		if (pt->_child[i]) {
+			objDraw(pt->_child[i]);
 		}
 	}
 }
