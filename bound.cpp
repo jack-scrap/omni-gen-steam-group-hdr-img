461739225535dc504b994893926c726ce7dbdb29
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-05-29 23:00:40 -0600

    Made bound indices static

--- bound.cpp
+++ bound.cpp
@@ -2,6 +2,26 @@
 #include "util.h"
 #include "math.h"
 
+static GLushort Bound::_idc[3 * 2 * 3 * 2] = {
+	0, 1, 2,
+	2, 1, 3,
+
+	4, 5, 6,
+	6, 5, 7,
+
+	0, 4, 1,
+	1, 4, 5,
+
+	2, 6, 3,
+	3, 6, 7,
+
+	0, 4, 2,
+	2, 4, 6,
+
+	1, 5, 3,
+	3, 5, 7
+}
+
 Bound* boundMk(Obj** obj, unsigned int sz) {
 	Bound* _ = (Bound*) malloc(sizeof (Bound));
 
@@ -31,27 +51,7 @@ Bound* boundMk(Obj** obj, unsigned int sz) {
 		}
 	}
 
-	GLushort idc[3 * 2 * 3 * 2] = {
-		0, 1, 2,
-		2, 1, 3,
-
-		4, 5, 6,
-		6, 5, 7,
-
-		0, 4, 1,
-		1, 4, 5,
-
-		2, 6, 3,
-		3, 6, 7,
-
-		0, 4, 2,
-		2, 4, 6,
-
-		1, 5, 3,
-		3, 5, 7
-	};
-
-	_->_parent = objMk(vtc, 2 * 2 * 2 * 3, idc, 3 * 2 * 3 * 2, "obj", "trans", true);
+	_->_parent = objMk(vtc, 2 * 2 * 2 * 3, Bound::_idc, 3 * 2 * 3 * 2, "obj", "trans", true);
 
 	return _;
 }
--- bound.h
+++ bound.h
@@ -6,6 +6,8 @@
 
 typedef struct {
 	Obj* _parent;
+
+	static GLushort _idc[3 * 2 * 3 * 2];
 } Bound;
 
 Bound* boundMk(Obj** obj, unsigned int sz);
