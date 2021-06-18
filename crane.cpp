06c53f0a538a6a240a091ca7abf2c2eccca011ed
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-06-17 20:35:13 -0600

    Temporarily removed crane's claw, existence of which when provided as child to head is causing segfault. To be refactored into Y component

--- crane.cpp
+++ crane.cpp
@@ -30,7 +30,7 @@ Crane* craneMk(Cont* init, glm::vec3 loc, glm::vec3 rot) {
 		objMk("crane/claw", "obj", "dir", true)
 	};
 
-	child[Crane::HEAD] = objMk("crane/head", "obj", "dir", true, claw, 1, glm::vec3(0.0, 13.8, 0.0));
+	child[Crane::HEAD] = objMk("crane/head", "obj", "dir", true, glm::vec3(0.0, 13.8, 0.0));
 
 	// data
 	if (_->_data) {
