83eeff8cc492e659da735791121a171d7124bd0c
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-09-16 15:27:55 -0600

    Rectified stride on Z

--- dict.cpp
+++ dict.cpp
@@ -16,7 +16,7 @@ Dict* dictMk(nlohmann::json deser, glm::vec3 loc, glm::vec3 rot) {
 
 	glm::vec3 start = glm::vec3(0.0, 0.0, layout::letter[Y] + (layout::margin * 2 * 2)) + glm::vec3(layout::overhead, 0.0, layout::overhead);
 
-	GLfloat szPair = layout::stride[Z] + (layout::margin * 2) + (layout::stroke * 2) + (layout::margin * 2) + (layout::stroke * 2) + (layout::letter[Y] + (layout::margin * 2)) + (layout::margin * 2 * 2);
+	GLfloat szPair = layout::stride[Z] + (layout::margin * 2) + (layout::stroke * 2) + (layout::letter[Y] + (layout::margin * 2)) + (layout::margin * 2 * 2);
 
 	unsigned int i = 0;
 	for (const auto& pair : deser.items()) {
