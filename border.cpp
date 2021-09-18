54c17b2a96daa6718e624d3fa457c04070554824
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-09-18 04:39:33 -0600

    Formatted

--- border.cpp
+++ border.cpp
@@ -127,49 +127,49 @@ Border* borderMk(glm::vec2 bound, Obj** child, unsigned int noChild, glm::vec3 l
 
 	GLushort rect[2 * 2 * 2 * 2];
 
-		// rect
-		int c = 0;
-		for (int q = 0; q < 2; q++) {
-			i = 0;
+	// rect
+	int c = 0;
+	for (int q = 0; q < 2; q++) {
+		i = 0;
 
-			for (int y = 0; y < 2; y++) {
-				for (int x = 0; x < 2; x++) {
-					rect[c] = (q * 2) + (y * 2) + i;
+		for (int y = 0; y < 2; y++) {
+			for (int x = 0; x < 2; x++) {
+				rect[c] = (q * 2) + (y * 2) + i;
 
-					i++;
-					c++;
-				}
-			}
-		}
-
-		for (int start = 0; start < 2; start++) {
-			for (int i = 0; i < 2 * 2; i++) {
-				rect[c] = start + (i * 2);
-
+				i++;
 				c++;
 			}
 		}
+	}
 
-		// strip
-		bool far = false;
+	for (int start = 0; start < 2; start++) {
+		for (int i = 0; i < 2 * 2; i++) {
+			rect[c] = start + (i * 2);
 
-		for (int q = 0; q < 2 * 2; q++) {
-			for (int t = 0; t < 2; t++) {
-				for (int i = 0; i < 3; i++) {
-					int start = (far * 2) + (2 * t);
-					int step = (i * (t ? -1 : 1));
-					int last = (3 * (t && i == 2));
+			c++;
+		}
+	}
 
-					int idx  = start + step + last;
+	// strip
+	bool far = false;
 
-					strip[((q * 2 * 3) + (t * 3)) + i] = rect[(q * (2 * 2)) + idx];
-				}
+	for (int q = 0; q < 2 * 2; q++) {
+		for (int t = 0; t < 2; t++) {
+			for (int i = 0; i < 3; i++) {
+				int start = (far * 2) + (2 * t);
+				int step = (i * (t ? -1 : 1));
+				int last = (3 * (t && i == 2));
+
+				int idx  = start + step + last;
+
+				strip[((q * 2 * 3) + (t * 3)) + i] = rect[(q * (2 * 2)) + idx];
 			}
 		}
+	}
 
-		_->_parent = objMk(vtc, strip, 3 * 2 * 2 * 2, "obj", "solid", true, child, noChild, loc, rot);
+	_->_parent = objMk(vtc, strip, 3 * 2 * 2 * 2, "obj", "solid", true, child, noChild, loc, rot);
 
-		return _;
+	return _;
 }
 
 void borderDel(Border* border) {
