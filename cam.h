031a1e2d14302ce923fe9b2a16035ecd708dc866
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-09-19 16:21:15 -0600

    Made format of initialized vector consistent, all adhering to "zero-initialization" syntax

--- cam.h
+++ cam.h
@@ -7,7 +7,7 @@ struct Cam {
 	glm::vec3 _scale = glm::vec3(50, 50, 50);
 
 	glm::vec3 _posPrev = _pos;
-	glm::vec3 _posDelta = glm::vec3(0.0, 0.0, 0.0);
+	glm::vec3 _posDelta = glm::vec3(0.0);
 
 	int _begin[2];
 	int _curr[2];
