e638810d47b99d0bfdc3b3fd58ea96b1968364e9
jackhasakeyboard <jackhasakeyboard@gmail.com>
2021-09-24 20:49:57 -0600

    Refactored logo in boot sequence to utilize mesh like 2D glyphs when drawing framebuffer

--- main.cpp
+++ main.cpp
@@ -632,75 +632,90 @@ int main(int argc, char** argv) {
 	PyList_Append(path, PyUnicode_FromString("."));
 
 	if (boot) {
-		GLuint vao;
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-
-		GLuint vbo;
-		glGenBuffers(1, &vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
 		GLfloat vtc[] = {
 			-0.26, -0.26,
 			0.16, -0.26,
-			0.06, -0.10,
 			-0.10, -0.10,
+			0.06, -0.10,
 
 			0.16, -0.26,
 			0.26, -0.16,
-			0.10, -0.06,
 			0.06, -0.10,
+			0.10, -0.06,
 
 			0.26, -0.16,
 			0.26, 0.26,
-			0.10, 0.10,
 			0.10, -0.06,
+			0.10, 0.10,
 
 			0.26, 0.26,
 			-0.16, 0.26,
-			-0.06, 0.10,
 			0.10, 0.10,
+			-0.06, 0.10,
 
 			-0.16, 0.26,
 			-0.26, 0.16,
-			-0.10, 0.06,
 			-0.06, 0.10,
+			-0.10, 0.06,
 
 			-0.10, 0.06,
 			-0.26, 0.16,
-			-0.26, -0.26,
-			-0.10, -0.10
+			-0.10, -0.10,
+			-0.26, -0.26
 		};
-		glBufferData(GL_ARRAY_BUFFER, sizeof vtc, vtc, GL_STATIC_DRAW);
 
-		/* shader */
+		GLushort idc[] = {
+			0, 1, 2,
+			2, 1, 3,
+
+			4, 5, 6,
+			6, 5, 7,
+
+			8, 9, 10,
+			10, 9, 11,
+
+			12, 13, 14,
+			14, 13, 15,
+
+			16, 17, 18,
+			18, 17, 19,
+
+			20, 21, 22,
+			22, 21, 23
+		};
+
+		Mesh* mesh = meshMk(vtc, idc, sizeof idc / sizeof *idc);
+
 		Prog prog("logo", "solid");
 
 		prog.use();
 
 		// attribute
+		glBindBuffer(GL_ARRAY_BUFFER, mesh->_id[Mesh::VBO]);
 		GLint attrPos = glGetAttribLocation(prog._id, "pos");
 		glVertexAttribPointer(attrPos, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
 		glEnableVertexAttribArray(attrPos);
 
 		// uniform
-		GLint uniActive = glGetUniformLocation(prog._id, "active");
 		GLint uniRes = glGetUniformLocation(prog._id, "res");
+		GLint uniActive = glGetUniformLocation(prog._id, "active");
 
-		// initialize
-		glUniform1ui(uniActive, true);
 		glUniform2i(uniRes, disp->_res[X], disp->_res[Y]);
+
+		glUniform1ui(uniActive, true);
 
 		prog.unUse();
 
 		// draw
 		disp->clear();
 
+		glBindVertexArray(mesh->_id[Mesh::VAO]);
 		prog.use();
 
-		glDrawArrays(GL_QUADS, 0, (2 + 3) * 2 * 2 * 3);
+		glDrawElements(GL_TRIANGLES, 6 * 2 * 3, GL_UNSIGNED_SHORT, (GLvoid*) 0);
 
 		prog.unUse();
+		glBindVertexArray(0);
 
 		disp->update();
 
