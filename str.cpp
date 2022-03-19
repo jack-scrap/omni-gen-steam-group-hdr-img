cfb2a71858f1cd51b19d0c9f8ffa108f314c5c35
Jack <jackhasakeyboard@gmail.com>
2022-03-19 00:56:23 -0600

    Spaced strings "proportionally" -- position of current character relative to all the ones that came before it. Process of doing so requires generation of first character that's the same as the head just to generate the offset

--- str.cpp
+++ str.cpp
@@ -8,9 +8,21 @@
 Str* strMk(std::string buff, glm::vec3 loc, glm::vec3 rot) {
 	Str* _ = (Str*) malloc(sizeof (Str));
 
+	Obj* head = objMk(std::string("glyph/") + buff[0], "obj", "solid", true);
+
+	float headWd = head->_aabb[X][MAX] - head->_aabb[X][MIN];
+
+	float offset = layout::padded(headWd);
+
+	objDel(head);
+
 	Obj* child[buff.size() - 1];
 	for (int i = 0; i < buff.size() - 1; i++) {
-		child[i] = objMk(std::string("glyph/") + buff[i + 1], "obj", "solid", true, glm::vec3((i + 1) * (layout::letter[X] + layout::stroke), 0.0, 0.0));
+		child[i] = objMk(std::string("glyph/") + buff[i + 1], "obj", "solid", true, glm::vec3(offset, 0.0, 0.0));
+
+		float wd = child[i]->_aabb[X][MAX] - child[i]->_aabb[X][MIN];
+
+		offset += layout::padded(wd);
 	}
 
 	_->_parent = objMk(std::string("glyph/") + buff[0], "obj", "solid", true, child, sizeof child / sizeof *child, loc);
