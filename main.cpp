477174b1824aa5489b0e4f40da4a5d460419bad4
Jack <jackhasakeyboard@gmail.com>
2022-04-06 07:35:45 -0600

    Utilized GLM function to clamp panning of camera

--- main.cpp
+++ main.cpp
@@ -511,8 +511,7 @@ void handle() {
 		}
 
 		if (e.type == SDL_MOUSEBUTTONUP) {
-			cam._pos = glm::vec3(cam._posPrev[X] + cam._posDelta[X] > 10000.0 + 500.0 ? 10000.0 + 500 : cam._posPrev[X] + cam._posDelta[X], cam._posPrev[Y] + cam._posDelta[Y] > 10000.0 + 500.0 ? 10000.0 + 500 : cam._posPrev[Y] + cam._posDelta[Y], cam._posPrev[Z] + cam._posDelta[Z] > 10000.0 + 500.0 ? 10000.0 + 500 : cam._posPrev[Z] + cam._posDelta[Z]);
-			cam._pos = glm::vec3(cam._pos[X] < 10000.0 - 500.0 ? 10000.0 - 500 : cam._pos[X], cam._pos[Y] < 10000.0 - 500.0 ? 10000.0 - 500 : cam._pos[Y], cam._pos[Z] < 10000.0 - 500.0 ? 10000.0 - 500 : cam._pos[Z]);
+			cam._pos = glm::clamp(cam._posPrev + cam._posDelta, glm::vec3(10000.0 - 500.0), glm::vec3(10000.0 + 500.0));
 
 			cam._drag = false;
 		}
@@ -530,8 +529,7 @@ void handle() {
 
 				cam._posDelta[Y] = -cam._delta[Y];
 
-				cam._pos = glm::vec3(cam._posPrev[X] + cam._posDelta[X] > 10000.0 + 500.0 ? 10000.0 + 500.0 : cam._posPrev[X] + cam._posDelta[X], cam._posPrev[Y] + cam._posDelta[Y] > 10000.0 + 500.0 ? 10000.0 + 500.0 : cam._posPrev[Y] + cam._posDelta[Y], cam._posPrev[Z] + cam._posDelta[Z] > 10000.0 + 500.0 ? 10000.0 + 500 : cam._posPrev[Z] + cam._posDelta[Z]);
-				cam._pos = glm::vec3(cam._pos[X] < 10000.0 - 500.0 ? 10000.0 - 500 : cam._pos[X], cam._pos[Y] < 10000.0 - 500.0 ? 10000.0 - 500 : cam._pos[Y], cam._pos[Z] < 10000.0 - 500.0 ? 10000.0 - 500 : cam._pos[Z]);
+				cam._pos = glm::clamp(cam._posPrev + cam._posDelta, glm::vec3(10000.0 - 500.0), glm::vec3(10000.0 + 500.0));
 			}
 		}
 
