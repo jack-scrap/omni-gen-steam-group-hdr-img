0cc8b1ef58cde77706814716d8f503e79a70f3a5
Jack <jackhasakeyboard@gmail.com>
2022-04-13 02:29:12 -0600

    Rectified rotation being passed to constructed objects as a constant by specifying in degrees

--- street_sign.cpp
+++ street_sign.cpp
@@ -10,7 +10,7 @@ StreetSign* streetSignMk(CArr pass, glm::vec3 loc, glm::vec3 rot) {
 
 	Obj* child[_->_pass._sz];
 	for (int i = 0; i < _->_pass._sz; i++) {
-		child[i] = objMk("street_sign/go", "obj", "solid", true, glm::vec3(layout::item(0.2 / 2), 6.0 + 1.0, i * 2.0), glm::vec3(0.0, M_PI / 2, 0.0));
+		child[i] = objMk("street_sign/go", "obj", "solid", true, glm::vec3(layout::item(0.2 / 2), 6.0 + 1.0, i * 2.0), glm::vec3(0.0, 90, 0.0));
 	}
 
 	_->_parent = objMk("street_sign/body", "obj", "dir", false, child, sizeof child / sizeof *child, loc, rot);
