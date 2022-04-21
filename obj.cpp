ecb31ef2d2051e5a87df65ba1538499529c1c778
Jack <jackhasakeyboard@gmail.com>
2022-04-21 07:37:50 -0600

    Moved loading of texture into lower level texture

--- obj.cpp
+++ obj.cpp
@@ -221,6 +221,26 @@ Obj* objMk(GLfloat* vtc, GLfloat* st, GLushort* idc, unsigned int noPrim, std::s
 	glVertexAttribPointer(_->_attr[Obj::ST], 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
 	glEnableVertexAttribArray(_->_attr[Obj::ST]);
 
+	// texture
+	glGenTextures(1, &_->_tex);
+	glBindTexture(GL_TEXTURE_2D, _->_tex);
+
+	int dim[2];
+	int chan;
+	unsigned char* data = stbi_load(util::fs::path::build({
+		"res",
+		tex
+	}).c_str(), &dim[X], &dim[Y], &chan, 0);
+
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, dim[X], dim[Y], 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+
 	glBindTexture(GL_TEXTURE_2D, 0);
 	_->_prog.unUse();
 	glBindVertexArray(0);
@@ -268,7 +288,6 @@ Obj* objMk(std::string name, std::string vtx, std::string frag, std::string tex,
 	}
 
 	glBindTexture(GL_TEXTURE_2D, 0);
-
 	_->_prog.unUse();
 	glBindVertexArray(0);
 
@@ -293,31 +312,6 @@ Obj* objMk(std::string name, std::string vtx, std::string frag, std::string tex,
 	}
 
 	Obj* _ = objMk(&vtc[0], &stIdxed[0], &idcVtc[0], idcVtc.size(), vtx, frag, tex, active, child, noChild, loc, rot);
-
-	// texture
-	glGenTextures(1, &_->_tex);
-	glBindTexture(GL_TEXTURE_2D, _->_tex);
-
-	int dim[2];
-	int chan;
-	unsigned char* data = stbi_load(util::fs::path::build({
-		"res",
-		tex
-	}).c_str(), &dim[X], &dim[Y], &chan, 0);
-
-	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, dim[X], dim[Y], 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	_->_prog.unUse();
-	glBindVertexArray(0);
 
 	return _;
 }
