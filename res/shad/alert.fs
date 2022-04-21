970361c7bcaa8b8d39227db8befe83bd45476607
Jack <jackhasakeyboard@gmail.com>
2022-04-20 20:24:43 -0600

    Made number of milliseconds integral in shaders

--- res/shad/alert.fs
+++ res/shad/alert.fs
@@ -12,7 +12,7 @@ vec3[2] col = vec3[2](
 
 void main() {
 	if (active) {
-		if (mod(_obj.x + _obj.z + _obj.y + (float(t) / 1000.0), 2.0) > 1.0) {
+		if (mod(_obj.x + _obj.z + _obj.y + (float(t) / 1000), 2.0) > 1.0) {
 			gl_FragColor = vec4(col[int(true)] / 255.0, 1.0);
 		} else {
 			discard;
