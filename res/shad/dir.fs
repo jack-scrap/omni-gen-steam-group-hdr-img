ed60ed7c9ba0e0984890a6f0f2e90ad483a3ca09
Jack <jackhasakeyboard@gmail.com>
2022-05-18 21:20:13 -0600

    Added ambient lighting

--- res/shad/dir.fs
+++ res/shad/dir.fs
@@ -4,6 +4,8 @@ in vec3 _pos;
 
 uniform bool active;
 
+const float ambi = 0.6;
+
 vec3 sun = vec3(1.0, 1.0, 1.0);
 
 vec3[2] col = vec3[2](
@@ -14,7 +16,7 @@ vec3[2] col = vec3[2](
 void main() {
 	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
 
-	float ins = max(dot(normFace, normalize(sun)), 0.0);
+	float ins = ambi + (max(dot(normFace, normalize(sun)), 0.0) * (1.0 - ambi));
 
 	gl_FragColor = vec4(ins * (col[int(active)] / 255.0), 1.0);
 }
--- res/shad/tex.fs
+++ res/shad/tex.fs
@@ -5,12 +5,14 @@ in vec2 _st;
 
 uniform sampler2D tex;
 
+const float ambi = 0.6;
+
 vec3 sun = vec3(1.0, 1.0, 1.0);
 
 void main() {
 	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
 
-	float ins = max(dot(normFace, normalize(sun)), 0.0);
+	float ins = ambi + (max(dot(normFace, normalize(sun)), 0.0) * (1.0 - ambi));
 
 	gl_FragColor = vec4(ins * texture(tex, _st).rgb, 1.0);
 }
