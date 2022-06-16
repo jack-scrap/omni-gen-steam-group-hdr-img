640081d32cfd86e482e9afd397b2731528435636
Jack <jackhasakeyboard@gmail.com>
2022-06-15 18:41:11 -0600

    Removed X component from alert shader on the grounds that the shader moves quicker when in a state of being transformed

--- res/shad/alert.fs
+++ res/shad/alert.fs
@@ -12,7 +12,7 @@ vec3[2] col = vec3[2](
 
 void main() {
 	if (active) {
-		if (mod(_pos.x + _pos.y + _pos.z + (float(t) / 1000), 2.0) > 1.0) {
+		if (mod(_pos.y + _pos.z + (float(t) / 1000), 2.0) > 1.0) {
 			gl_FragColor = vec4(col[int(true)] / 255.0, 1.0);
 		} else {
 			discard;
