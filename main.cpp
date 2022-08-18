9aecd207333b679703ce19e9e590fff080d03f37
Jack <jackhasakeyboard@gmail.com>
2022-08-18 01:13:11 -0600

    Drew boot sequence unconditionally

--- main.cpp
+++ main.cpp
@@ -728,98 +728,96 @@ int main(int argc, char* argv[]) {
 	PyList_Append(path, PyUnicode_FromString("."));
 	PyList_Append(path, PyUnicode_FromString("player/script"));
 
-	if (boot) {
-		GLfloat vtc[] = {
-			-0.26, 0.0, -0.26,
-			0.16, 0.0, -0.26,
-			-0.10, 0.0, -0.10,
-			0.06, 0.0, -0.10,
+	GLfloat vtc[] = {
+		-0.26, 0.0, -0.26,
+		0.16, 0.0, -0.26,
+		-0.10, 0.0, -0.10,
+		0.06, 0.0, -0.10,
 
-			0.16, 0.0, -0.26,
-			0.26, 0.0, -0.16,
-			0.06, 0.0, -0.10,
-			0.10, 0.0, -0.06,
+		0.16, 0.0, -0.26,
+		0.26, 0.0, -0.16,
+		0.06, 0.0, -0.10,
+		0.10, 0.0, -0.06,
 
-			0.26, 0.0, -0.16,
-			0.26, 0.0, 0.26,
-			0.10, 0.0, -0.06,
-			0.10, 0.0, 0.10,
+		0.26, 0.0, -0.16,
+		0.26, 0.0, 0.26,
+		0.10, 0.0, -0.06,
+		0.10, 0.0, 0.10,
 
-			0.26, 0.0, 0.26,
-			-0.16, 0.0, 0.26,
-			0.10, 0.0, 0.10,
-			-0.06, 0.0, 0.10,
+		0.26, 0.0, 0.26,
+		-0.16, 0.0, 0.26,
+		0.10, 0.0, 0.10,
+		-0.06, 0.0, 0.10,
 
-			-0.16, 0.0, 0.26,
-			-0.26, 0.0, 0.16,
-			-0.06, 0.0, 0.10,
-			-0.10, 0.0, 0.06,
+		-0.16, 0.0, 0.26,
+		-0.26, 0.0, 0.16,
+		-0.06, 0.0, 0.10,
+		-0.10, 0.0, 0.06,
 
-			-0.10, 0.0, 0.06,
-			-0.26, 0.0, 0.16,
-			-0.10, 0.0, -0.10,
-			-0.26, 0.0, -0.26
-		};
+		-0.10, 0.0, 0.06,
+		-0.26, 0.0, 0.16,
+		-0.10, 0.0, -0.10,
+		-0.26, 0.0, -0.26
+	};
 
-		GLushort idc[] = {
-			0, 1, 2,
-			2, 1, 3,
+	GLushort idc[] = {
+		0, 1, 2,
+		2, 1, 3,
 
-			4, 5, 6,
-			6, 5, 7,
+		4, 5, 6,
+		6, 5, 7,
 
-			8, 9, 10,
-			10, 9, 11,
+		8, 9, 10,
+		10, 9, 11,
 
-			12, 13, 14,
-			14, 13, 15,
+		12, 13, 14,
+		14, 13, 15,
 
-			16, 17, 18,
-			18, 17, 19,
+		16, 17, 18,
+		18, 17, 19,
 
-			20, 21, 22,
-			22, 21, 23
-		};
+		20, 21, 22,
+		22, 21, 23
+	};
 
-		Mesh* mesh = meshMk(vtc, idc, sizeof idc / sizeof *idc);
+	Mesh* mesh = meshMk(vtc, idc, sizeof idc / sizeof *idc);
 
-		Prog prog("logo", "solid");
+	Prog prog("logo", "solid");
 
-		prog.use();
+	prog.use();
 
-		// attribute
-		glBindBuffer(GL_ARRAY_BUFFER, mesh->_id[Mesh::VBO]);
-		GLint attrPos = glGetAttribLocation(prog._id, "pos");
-		glVertexAttribPointer(attrPos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
-		glEnableVertexAttribArray(attrPos);
+	// attribute
+	glBindBuffer(GL_ARRAY_BUFFER, mesh->_id[Mesh::VBO]);
+	GLint attrPos = glGetAttribLocation(prog._id, "pos");
+	glVertexAttribPointer(attrPos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
+	glEnableVertexAttribArray(attrPos);
 
-		// uniform
-		GLint uniRes = glGetUniformLocation(prog._id, "res");
-		GLint uniActive = glGetUniformLocation(prog._id, "active");
+	// uniform
+	GLint uniRes = glGetUniformLocation(prog._id, "res");
+	GLint uniActive = glGetUniformLocation(prog._id, "active");
 
-		glUniform2ui(uniRes, disp->_res[X], disp->_res[Y]);
+	glUniform2ui(uniRes, disp->_res[X], disp->_res[Y]);
 
-		glUniform1ui(uniActive, true);
+	glUniform1ui(uniActive, true);
 
-		prog.unUse();
+	prog.unUse();
 
-		// draw
-		disp->clear();
+	// draw
+	disp->clear();
 
-		glBindVertexArray(mesh->_id[Mesh::VAO]);
-		prog.use();
+	glBindVertexArray(mesh->_id[Mesh::VAO]);
+	prog.use();
 
-		glDrawElements(GL_TRIANGLES, 6 * 2 * 3, GL_UNSIGNED_SHORT, (GLvoid*) 0);
+	glDrawElements(GL_TRIANGLES, 6 * 2 * 3, GL_UNSIGNED_SHORT, (GLvoid*) 0);
 
-		prog.unUse();
-		glBindVertexArray(0);
+	prog.unUse();
+	glBindVertexArray(0);
 
-		meshDel(mesh);
+	meshDel(mesh);
 
-		disp->update();
+	disp->update();
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-	}
+	std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 
 	return 0;
 }
